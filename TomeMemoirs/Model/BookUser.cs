7f2492f163c424449ec159ce7c85e01464f803b0
Luna <93603497+TheLunaWitch@users.noreply.github.com>
2024-03-26 08:57:57 +0100

    Klasses toevoegen

---
+++ TomeMemoirs/Model/BookUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomeMemoirs.Model
+{
+    public class BookUser
+    {
+        public int Id { get; set; }
+        public User User { get; set; } //Refers to User model
+        public int UserId { get; set; } //Uses reference to get the User Id
+        public Book Book { get; set; } //Refers to Book model
+        public int BookId { get; set; } //Uses reference to get Book id
+        public int Rating { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ TomeMemoirs/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomeMemoirs.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string HashedPassWord { get; set; }
+        public static User LoggedInUser { get; set; } //Refers to the current logged in user
+    }
+}
