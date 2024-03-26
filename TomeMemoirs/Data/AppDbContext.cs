e62e0d173a713362e2a5dd344ca198a86245584b
Luna <93603497+TheLunaWitch@users.noreply.github.com>
2024-03-26 09:00:26 +0100

    Added connection database
    Database connection made and classes added

---
+++ TomeMemoirs/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomeMemoirs.Model;
+
+namespace TomeMemoirs.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BookUser> BookUsers { get; set; }
+
+        //Connect Database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;" +
+                "port=3306;" +
+                "user=c_sharp;" +
+                "password=Krijnisleider;" +
+                "database=Db_TomeMemoir",
+                ServerVersion.Parse("8.0.30")
+            );
+        }
+    }
+}
